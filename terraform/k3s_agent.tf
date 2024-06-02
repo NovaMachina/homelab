f55b267d3d5f6cad1654fcda62ba9a04b7972b65
Jacob Williams <jacob.t.williams@outlook.com>
2024-06-01 20:45:34 -0500

    Disable rook

--- terraform/k3s_agent.tf
+++ terraform/k3s_agent.tf
@@ -18,7 +18,7 @@ resource "proxmox_vm_qemu" "k3s-agent-node" {
     numa = true
     vcpus = 0
     cpu = "host"
-    memory = 8192
+    memory = 12288
     name = "k3s-agent-0${count.index + 1}"
 
     cloudinit_cdrom_storage = "local-lvm"
