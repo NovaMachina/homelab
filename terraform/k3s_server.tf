458b3dfa2a57eac25e882f92677aa0decd502663
Jacob Williams <jacob.t.williams@outlook.com>
2024-06-06 20:47:33 -0500

    Restore openebs

--- terraform/k3s_server.tf
+++ terraform/k3s_server.tf
@@ -1,64 +1,70 @@
 resource "proxmox_vm_qemu" "k3s-server-node" {
-    # Node name has to be the same name as within the cluster
-    # this might not include the FQDN
-    target_node = "pve"
-    desc = "k3s Server ${count.index + 1}"
-    count = 3
-    onboot = true
+  # Node name has to be the same name as within the cluster
+  # this might not include the FQDN
+  target_node = "pve"
+  desc        = "k3s Server ${count.index + 1}"
+  count       = 3
+  onboot      = true
 
-    # The template name to clone this vm from
-    clone = "debian12-cloudinit-template"
+  # The template name to clone this vm from
+  clone = "debian12-cloudinit-template"
 
-    # Activate QEMU agent for this VM
-    agent = 1
+  # Activate QEMU agent for this VM
+  agent = 1
 
-    os_type = "cloud-init"
-    cores = 2
-    sockets = 2
-    numa = true
-    vcpus = 0
-    cpu = "host"
-    memory = 8192
-    name = "k3s-server-0${count.index + 1}"
+  os_type = "cloud-init"
+  cores   = 2
+  sockets = 2
+  numa    = true
+  vcpus   = 0
+  cpu     = "host"
+  memory  = 8192
+  name    = "k3s-server-0${count.index + 1}"
 
-    cloudinit_cdrom_storage = "local-lvm"
-    scsihw   = "virtio-scsi-single" 
-    bootdisk = "scsi0"
+  scsihw   = "virtio-scsi-single"
+  bootdisk = "scsi0"
 
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                  storage = "local-lvm"
-                  size = "32"
-                  emulatessd = true
-                }
-            }
+  disks {
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local-lvm"
         }
+      }
     }
-
-    network {
-        model = "virtio"
-        bridge = "vmbr1"
-        tag = 400
+    scsi {
+      scsi0 {
+        disk {
+          storage    = "local-lvm"
+          size       = "32"
+          emulatessd = true
+        }
+      }
     }
+  }
 
-    ipconfig0 = "ip=10.0.40.10${count.index + 1}/24,gw=10.0.40.1"
+  network {
+    model  = "virtio"
+    bridge = "vmbr1"
+    tag    = 400
+  }
 
-    ciuser = "debian"
+  ipconfig0 = "ip=10.0.40.10${count.index + 1}/24,gw=10.0.40.1"
 
-    connection {
-      type = "ssh"
-      agent = true
-      host = "10.0.40.10${count.index + 1}"
-      user = "debian"
-      private_key = file(pathexpand("~/.ssh/id_rsa"))
-    }
+  ciuser = "debian"
 
-    provisioner "remote-exec" {
-        inline = [ 
-            "echo ${var.sudo_password} | sudo -S rm -rf /etc/machine-id",
-            "echo ${var.sudo_password} | sudo -S dbus-uuidgen --ensure=/etc/machine-id",
-         ]
-    }
+  connection {
+    type        = "ssh"
+    agent       = true
+    host        = "10.0.40.10${count.index + 1}"
+    user        = "debian"
+    private_key = file(pathexpand("~/.ssh/id_rsa"))
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo ${var.sudo_password} | sudo -S rm -rf /etc/machine-id",
+      "echo ${var.sudo_password} | sudo -S dbus-uuidgen --ensure=/etc/machine-id",
+    ]
+  }
 }
--- terraform/k3s_storage.tf
+++ terraform/k3s_storage.tf
@@ -1,78 +1,83 @@
 resource "proxmox_vm_qemu" "k3s-storage-node" {
-    # Node name has to be the same name as within the cluster
-    # this might not include the FQDN
-    depends_on = [ proxmox_vm_qemu.k3s-server-node, proxmox_vm_qemu.k3s-agent-node ]
-    
-    target_node = "pve"
-    desc = "k3s Storage Node ${count.index + 1}"
-    count = 3
-    onboot = true
+  # Node name has to be the same name as within the cluster
+  # this might not include the FQDN
+  depends_on  = [proxmox_vm_qemu.k3s-server-node, proxmox_vm_qemu.k3s-agent-node]
+  target_node = "pve"
+  desc        = "k3s Storage Node ${count.index + 1}"
+  count       = 3
+  onboot      = true
 
-    # The template name to clone this vm from
-    clone = "debian12-cloudinit-template"
+  # The template name to clone this vm from
+  clone = "debian12-cloudinit-template"
 
-    # Activate QEMU agent for this VM
-    agent = 1
+  # Activate QEMU agent for this VM
+  agent = 1
 
-    os_type = "cloud-init"
-    cores = 3
-    sockets = 2
-    numa = true
-    vcpus = 0
-    cpu = "host"
-    memory = 16384
-    name = "k3s-storage-0${count.index + 1}"
+  os_type = "cloud-init"
+  cores   = 3
+  sockets = 2
+  numa    = true
+  vcpus   = 0
+  cpu     = "host"
+  memory  = 16384
+  name    = "k3s-storage-0${count.index + 1}"
 
-    cloudinit_cdrom_storage = "local-lvm"
-    scsihw   = "virtio-scsi-single" 
-    bootdisk = "scsi0"
+  scsihw                  = "virtio-scsi-single"
+  bootdisk                = "scsi0"
 
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                  storage = "local-lvm"
-                  size = "32"
-                  emulatessd = true
-                }
-            }
-            scsi1 {
-              disk {
-                storage = "TrueNAS"
-                size = "512"
-              }
-            }
+  disks {
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local-lvm"
         }
+      }
     }
-
-    network {
-        model = "virtio"
-        bridge = "vmbr1"
-        tag = 400
+    scsi {
+      scsi0 {
+        disk {
+          storage    = "local-lvm"
+          size       = "32"
+          emulatessd = true
+        }
+      }
+      scsi1 {
+        disk {
+          storage = "TrueNAS"
+          size    = "512"
+        }
+      }
     }
+  }
 
-    ipconfig0 = "ip=10.0.40.10${count.index + 7}/24,gw=10.0.40.1"
+  network {
+    model  = "virtio"
+    bridge = "vmbr1"
+    tag    = 400
+  }
 
-    ciuser = "debian"
+  ipconfig0 = "ip=10.0.40.10${count.index + 7}/24,gw=10.0.40.1"
 
-    timeouts {
-      create = "2h"
-      update = "2h"
-      delete = "20m"
-    }
+  ciuser = "debian"
 
-    connection {
-      type = "ssh"
-      agent = true
-      host = "10.0.40.10${count.index + 7}"
-      user = "debian"
-      private_key = file(pathexpand("~/.ssh/id_rsa"))
-    }
+  timeouts {
+    create = "2h"
+    update = "2h"
+    delete = "20m"
+  }
 
-    provisioner "remote-exec" {
-        inline = [ 
-            "echo ${var.sudo_password} | sudo -S rm -rf /etc/machine-id",
-            "echo ${var.sudo_password} | sudo -S dbus-uuidgen --ensure=/etc/machine-id",
-         ]
-    }
+  connection {
+    type        = "ssh"
+    agent       = true
+    host        = "10.0.40.10${count.index + 7}"
+    user        = "debian"
+    private_key = file(pathexpand("~/.ssh/id_rsa"))
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo ${var.sudo_password} | sudo -S rm -rf /etc/machine-id",
+      "echo ${var.sudo_password} | sudo -S dbus-uuidgen --ensure=/etc/machine-id",
+    ]
+  }
 }
